7d20510980b3048393ecff89d8d7dee1495ec701
Jarek_k <kolasa.jarek@gmail.com>
2018-07-07 14:29:26 +0200

    Data updates - getting actual value of Euro added

--- src/main/java/awyss/data/DataController.java
+++ src/main/java/awyss/data/DataController.java
@@ -1,19 +1,13 @@
 package awyss.data;
 
-import awyss.account.Account;
-import awyss.account.Role;
+import awyss.data.euro.EuroService;
 import awyss.data.hourlyWage.HourlyWage;
 import awyss.data.hourlyWage.HourlyWageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class DataController {
@@ -21,13 +15,15 @@ public class DataController {
     @Autowired
     private HourlyWageRepository hourlyWageRepository;
 
+    @Autowired
+    private EuroService euroService;
+
     @GetMapping("/data")
-    public String signin(Model model) {
+    public String jobList(Model model) {
         List<HourlyWage> hourlyWages = hourlyWageRepository.findAll();
         model.addAttribute("hourlyWage", hourlyWages);
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        authentication.getAuthorities();
-//        data.addAttribute("data", data);
+        model.addAttribute("euro", euroService);
         return "data/data";
     }
+
 }
